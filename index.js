d2a731cdefcc3cec87fbcae1e81bba9959857c87
MUCUGUR <mucahit.ugur@mercedes-benz.com>
2025-05-16 12:20:58 +0300

    refactor: implement env variables for DB connection

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const app = express();
-var cors = require('cors')
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
 
@@ -10,10 +14,26 @@ app.use(bodyParser.json());
 app.use(cors())
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
+
+async function initDb() {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS calculations (
+      id SERIAL PRIMARY KEY,
+      number1 INTEGER,
+      number2 INTEGER,
+      result INTEGER
+    );
+  `);
+    console.log("Database schema initialized");
+}
+
+initDb().catch(console.error);
 
 app.get('/', (req, res) => {
 
@@ -25,10 +45,12 @@ app.get('/', (req, res) => {
 
 app.post('/add', async (req, res) => {
     const { first, second } = req.body;
-    const sum = first + second;
+    const number1 = parseInt(first);
+    const number2 = parseInt(second);
+    const result = first + second;
     try {
-        // await pool.query('INSERT INTO calculations (a, b, sum) VALUES ($1, $2, $3)', [ a, b, sum ]);
-        res.json({ sum });
+        await pool.query('INSERT INTO calculations (number1, number2, result) VALUES ($1, $2, $3)', [ number1, number2, result ]);
+        res.json({ sum: result });
     } catch (err) {
         console.error(err);
         res.status(500).send('Error storing result');
